7d23f0a083a4688f74fd27adc9079e223fed8527
admin <xiaxuno1@163.com>
2022-03-01 20:07:25 +0800

    restore

---
+++ base/virtual_chatroom2.py
@@ -0,0 +1,9 @@
+# --------------------------------------------------
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+# PN: virtual_chatroom
+# FN: virtual_chatroom2
+# Author: xiaxu
+# DATA: 2022/3/1
+# Description:
+# ---------------------------------------------------
